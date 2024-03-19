1c979b887dde4254d3860443f0b2f7fddcdf8844
nika.ivanushko2014 <nika.ivanushko2014@gmail.com>
2024-03-19 21:22:27 +0100

    Add test that fails

--- src/test/java/com/application/tests/MainPageTests.java
+++ src/test/java/com/application/tests/MainPageTests.java
@@ -15,4 +15,13 @@ public class MainPageTests extends BaseTest {
         Assert.assertTrue(mainPage.isLoaded(), "Main page is not loaded");
     }
 
+    @Parameters("MainPageUrl")
+    @Test()
+    public void verifyPageIsOpened(String mainPageUrl){
+        MainPage mainPage = new MainPage(driver);
+
+        mainPage.navigateTo(mainPageUrl);
+        Assert.assertTrue(false, "Page is not loaded");
+    }
+
 }

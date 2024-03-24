b3a69838a2c1fce87abd4df5f8d6281f3cff79d0
nika.ivanushko2014 <nika.ivanushko2014@gmail.com>
2024-03-24 20:11:23 +0100

    Set listener

---
+++ src/main/java/com/application/core/listeners/TestNGListener.java
@@ -0,0 +1,55 @@
+package com.application.core.listeners;
+
+
+import org.apache.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.apache.log4j.LogManager;
+
+public class TestNGListener implements ITestListener {
+
+    private Logger log;
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        this.log = LogManager.getLogger(result.getMethod().getMethodName());
+        log.info(result.getMethod().getMethodName() + " Log on fail");
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        this.log = LogManager.getLogger(result.getMethod().getMethodName());
+        log.info(result.getMethod().getMethodName() + " Log on skipped");
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        this.log = LogManager.getLogger(result.getMethod().getMethodName());
+        log.info(result.getMethod().getMethodName() + " Log on fail with success percentage");
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        this.log = LogManager.getLogger(result.getMethod().getMethodName());
+        log.info(result.getMethod().getMethodName() + " Log on fail with timeout");
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        this.log = LogManager.getLogger(context.getName());
+        log.info(context.getName() + " Log on start");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        this.log = LogManager.getLogger(context.getName());
+        log.info(context.getName() +  " Log on finish");
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        log.info(result.getMethod().getMethodName() + " Log on test start");
+    }
+}
